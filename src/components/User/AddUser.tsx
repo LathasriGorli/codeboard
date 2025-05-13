e1de2e4e0d7f76246d982ac8b73372e6f3eae83f
sudhashini406-cmd <sudhashini.e@orotron.com>
2025-05-13 09:56:03 +0530

    update the add user card

--- src/components/User/AddUser.tsx
+++ src/components/User/AddUser.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
+
+import { format } from "date-fns";
+import { Calendar } from "../ui/calendar";
+import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 import { Button } from "../ui/button";
+import { CalendarIcon } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -10,14 +15,18 @@ import {
 } from "../ui/card";
 import { Label } from "../ui/label";
 export function CardWithAddUser() {
+  const [dob, setDob] = React.useState<Date>();
+  const [doj, setDoj] = React.useState<Date>();
   return (
     <Card className="w-[900px] mx-10 my-10 shadow-md  bg-(--an-color-text-Adduser) ">
       <CardHeader>
-        <CardTitle className="font-(family-name:--an-card-font) text-(--an-color-text-cardtitlec) text-(length:--an-text-cardtitle-font-sizec) leading-(--an-text-cardtitle-line-heightc) font-(--an-text-cardtitle-font-weightc)  ">
+        <CardTitle className="flex">
+          <div className="font-[urbanist] text-(--an-color-text-cardtitlec) text-(length:--an-text-cardtitle-font-sizec) leading-(--an-text-cardtitle-line-heightc) font-(--an-text-cardtitle-font-weightc) ">
           Add User
+          </div>
         </CardTitle>
         <CardDescription className="capitalize mt-8 ">
-          <div className="font-(family-name:--an-card-font) text-(--an-color-text-cardtitlec) text-(length:--an-text-cardD-font-size) leading-(--an-text-cardD-line-height) font-(--an-text-cardD-line-height) ">
+          <div className="font-[urbanist] text-(--an-color-text-cardtitlec) text-(length:--an-text-cardD-font-size) leading-(--an-text-cardD-line-height) font-(--an-text-cardtitle-font-weightc) ">
             Personal Details
           </div>
         </CardDescription>
@@ -27,7 +36,7 @@ export function CardWithAddUser() {
           <div className="grid grid-cols-2 gap-x-8 gap-y-6">
             <div>
               <Label htmlFor="firstName">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight) ">
+                <div className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight) ">
                   First Name
                 </div>
               </Label>
@@ -38,7 +47,7 @@ export function CardWithAddUser() {
             </div>
             <div>
               <Label htmlFor="lastName">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+                <div className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   Last Name
                 </div>
               </Label>
@@ -49,7 +58,7 @@ export function CardWithAddUser() {
             </div>
             <div>
               <Label htmlFor="email">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+                <div className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   Email Address
                 </div>
               </Label>
@@ -61,7 +70,7 @@ export function CardWithAddUser() {
             </div>
             <div>
               <Label htmlFor="mobile">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+                <div className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   Mobile Number
                 </div>
               </Label>
@@ -71,34 +80,71 @@ export function CardWithAddUser() {
               />
             </div>
             <div>
-              <Label htmlFor="dob">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+              <Label htmlFor="dob" className="capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   DOB
-                </div>
               </Label>
-              <input
-                id="dob"
-                type="date"
-                placeholder="mm/dd/yyyy"
-                className="w-full h-7 px-2 border-b border-black/10 focus:outline-none"
-              />
+              <div className="relative w-full">
+                <input
+                  readOnly
+                  value={dob ? format(dob, "PPP") : ""}
+                  placeholder="mm/dd/yyyy"
+                  className="w-full h-9 px-3 pr-10 border-b border-black/10 focus:outline-none font-[ urbanist] text-(--an-text-doj-color) text-(length:--an-text-firstN-font-size) font-(--an-text-cardtitle-font-weightc) leading-(--an-text-firstN-line-height)"
+                />
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <button
+                      type="button"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500"
+                    >
+                      <CalendarIcon className="h-4 w-4" />
+                    </button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="end">
+                    <Calendar
+                      mode="single"
+                      selected={dob}
+                      onSelect={setDob}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
             <div>
-              <Label htmlFor="doj">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+              <Label htmlFor="doj" className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   Date of Joining
-                </div>
               </Label>
-              <input
-                id="doj"
-                type="date"
-                placeholder=" mm/dd/yyyy"
-                className="w-full h-7 px-2 border-b border-black/10 focus:outline-none"
-              />
+              <div className="relative w-full">
+                <input
+                  readOnly
+                  value={doj ? format(doj, "PPP") : ""}
+                  placeholder="mm/dd/yyyy"
+                  className="w-full h-9 px-3 pr-10 border-b border-black/10 focus:outline-none font-[urbanist] text-(--an-text-doj-color) text-(length:--an-text-firstN-font-size) font-(--an-text-cardtitle-font-weightc) leading-(--an-text-firstN-line-height)"
+                />
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <button
+                      type="button"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500"
+                    >
+                      <CalendarIcon className="h-4 w-4" />
+                    </button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="end">
+                    <Calendar
+                      mode="single"
+                      selected={doj}
+                      onSelect={setDoj}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
+
             <div className="col-span-2">
               <Label htmlFor="designation">
-                <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+                <div className=" capitalize font-[urbanist] text-(--an-color-text-firstN) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
                   Designation
                 </div>
               </Label>
@@ -112,12 +158,12 @@ export function CardWithAddUser() {
       </CardContent>
       <CardFooter className="flex justify-end gap-4">
         <Button variant="outline">
-          <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-cardfooter) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+          <div className=" capitalize font-[urbanist] text-(--an-color-text-cardfooter) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
             Cancel
           </div>
         </Button>
         <Button className="bg-red-600 text-white hover:bg-red-700">
-          <div className=" capitalize font-(family-name:--an-card-font) text-(--an-color-text-Adduser) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
+          <div className=" capitalize font-[urbanist] text-(--an-color-text-Adduser) text-(length:--an-text-firstN-font-size) leading-(--an-text-firstN-line-height) font-(--an-text-firstN-font-weight)">
             Add User
           </div>
         </Button>
